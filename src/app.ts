7c21aff22495a120a1535c34a79462c0e75adc8c
Marcus Sanatan <msanatan@gmail.com>
2024-10-27 21:34:07 -0400

    Separate app set up logic from server running logic

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import { meetingRoutes } from "./routes/meetings.js";
+import { taskRoutes } from "./routes/task.router.js";
+import { dashboardRoutes } from "./routes/dashboardRoutes.js";
+import { authMiddleware } from "./auth.middleware.js";
+
+const app = express();
+
+await mongoose
+  .connect("mongodb://localhost:27017/meetingbot")
+  .then((conn) => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the MeetingBot API" });
+});
+
+app.use("/api/meetings", authMiddleware, meetingRoutes);
+app.use("/api/tasks", authMiddleware, taskRoutes);
+app.use("/api/dashboard", authMiddleware, dashboardRoutes);
+
+export default app;
