b5c39554efbb7510595b01151cdfb51d3c02f5d9
Marcus Sanatan <msanatan@gmail.com>
2024-10-27 23:05:08 -0400

    Fix import of task controller in routes

--- src/services/tasks/task.routes.ts
+++ src/services/tasks/task.routes.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import { getAllTasks } from "./task.controller.ts";
+import { getAllTasks } from "./task.controller.js";
 
 export const router = express.Router();
-
-
 
 router.get("/", getAllTasks);
 export { router as taskRoutes };
