5addf53da418f43f2eae340c3f00edcbd0edc2dd
Marcus Sanatan <msanatan@gmail.com>
2024-10-28 01:14:14 -0400

    Update jest configs so we can import from the src directory
    Taken from: https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
    
    Full discussion on GitHub here: https://github.com/jest-community/vscode-jest/issues/382

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 export default {
-  moduleFileExtensions: ["ts", "js"],
   preset: "ts-jest",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  moduleFileExtensions: ["ts", "js"],
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
   transformIgnorePatterns: ["/node_modules/"],
