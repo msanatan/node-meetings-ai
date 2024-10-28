1aa7dde1a4d13b44dcf327e922ce43f05ae3ca54
Marcus Sanatan <msanatan@gmail.com>
2024-10-28 01:40:56 -0400

    First test passes!

--- __tests__/integration/meetings.test.ts
+++ __tests__/integration/meetings.test.ts
@@ -1,9 +1,16 @@
 import request from "supertest";
 import app from "../../src/app.js";
 import { connectDB, disconnectDB } from "../../src/db.js";
+import { generateToken } from "../../src/utils/token.js";
+
+let authToken: string | null = null;
 
 beforeAll(async () => {
   await connectDB();
+  // Set the JWT secret
+  process.env.JWT_SECRET = "test-secret";
+  // Generate a token
+  authToken = generateToken("user5", process.env.JWT_SECRET);
 });
 
 afterAll(async () => {
@@ -12,7 +19,10 @@ afterAll(async () => {
 
 describe("Meetings", () => {
   it("should get all meetings", async () => {
-    const response = await request(app).get("/api/meetings");
+    const response = await request(app)
+      .get("/api/meetings")
+      .set("Accept", "application/json")
+      .set("Authorization", `Bearer ${authToken}`);
     expect(response.status).toBe(200);
     const data = response.body.data;
     expect(Array.isArray(data)).toBe(true);
