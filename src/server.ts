1c9154d84c024df5bbc4a54172f946c29b430cd3
Marcus Sanatan <msanatan@gmail.com>
2024-11-04 04:57:13 -0400

    Connect and disconnect to Redis at start up

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
 import app from "./app";
 import { config } from "./config";
-import { connectDB } from "./db";
-import logger from "./logger";
+import { connectDB, disconnectDB } from "./db.js";
+import { connectRedis, disconnectRedis } from "./utils/redisClient.js";
+import logger from "./logger.js";
 
 async function main() {
   await connectDB();
+  await connectRedis();
 
-  app.listen(config.port, () => {
+  const server = app.listen(config.port, () => {
     logger.info(`Server is running on port ${config.port}`);
   });
+
+  // Handle graceful shutdown
+  const shutdown = async () => {
+    logger.info("Shutting down server...");
+    server.close(async () => {
+      await disconnectDB();
+      await disconnectRedis();
+      logger.info("Server shut down gracefully");
+      process.exit(0);
+    });
+  };
+
+  process.on("SIGINT", shutdown);
+  process.on("SIGTERM", shutdown);
 }
 
 main();
